bf2fd77f3b3dd1492c76c0c1e289d21843436980
Felix Ableitner <me@nutomic.com>
2022-05-17 14:30:26 +0200

    Add strum feature for iterating over languages

--- iso639-1/src/lib.rs
+++ iso639-1/src/lib.rs
@@ -43,6 +43,9 @@
 #[cfg(feature = "serde")]
 extern crate serde;
 
+#[cfg(feature = "strum")]
+extern crate strum_macros;
+
 #[cfg(feature = "serde")]
 mod serde_impl;
 
@@ -121,6 +124,7 @@ impl From<Iso639v1ErrorKind> for Iso639v1Error {
 /// }
 /// ```
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[cfg_attr(feature = "strum", derive(strum_macros::EnumIter))]
 pub enum Iso639_1 {
     /// 639-2: aar, name: Afar (Afaraf)
     Aa,
